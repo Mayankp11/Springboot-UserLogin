17c849c046e4d4c971aa21813a2a1a78e0e145a6
Mayank <mayankpatil2605@gmail.com>
2024-12-10 14:37:54 -0800

    Added HttpServletResponse setHeader method to not store cache

--- src/main/java/com/techsorcerer/userLogin/controller/WelcomeController.java
+++ src/main/java/com/techsorcerer/userLogin/controller/WelcomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
@@ -12,7 +13,12 @@ import jakarta.servlet.http.HttpSession;
 public class WelcomeController {
 
 	@GetMapping
-	public String welcomePage(HttpSession httpSession, Model model) {
+	public String welcomePage(HttpSession httpSession,HttpServletResponse response ,Model model) {
+		// 
+		response.setHeader("Cache-Control", "no-store,max-age=0,must-revalidate");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		
 		String username = (String) httpSession.getAttribute("username");
 		
 		if(username == null) {
